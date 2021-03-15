e80f5c27384ea1f198cb72286291df55020c3ce2
yyamada12 <12yacropolisy@gmail.com>
2021-03-15 19:36:31 +0900

    Properties

--- Properties/Lazy property/src/Task.kt
+++ Properties/Lazy property/src/Task.kt
@@ -1,7 +1,10 @@
 class LazyProperty(val initializer: () -> Int) {
-    /* TODO */
+    var a: Int? = null
     val lazy: Int
         get() {
-            TODO()
+            if (a == null) {
+                a = initializer()
+            }
+            return a!!
         }
 }
